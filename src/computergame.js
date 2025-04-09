7022d9d8f3611285faa7b9c850e339287589169c
Keith Estrella <keithestrella@gmail.com>
2025-04-09 03:10:48 -0400

    Add P1 infoboard and css styling

--- src/computergame.js
+++ src/computergame.js
@@ -51,15 +51,50 @@ function P2TileMaker(index){
     p2Board.appendChild(tile);
 }
 
+function P1InfoPlacer(p1){
+    let infoName = document.querySelector("#p1-name");
+    infoName.textContent = p1.name;
+
+    let p1gameboard = p1.gameboard;
+    let p1ships = p1gameboard.ships;
+
+    for(let i = 0; i < p1ships.length; i++) {
+        P1InfoShipPlacer(p1ships[i].shipname);
+    }
+}
+
+function P1InfoShipPlacer(inputName){
+    let infoShip = document.querySelector("#p1-ships");
+    let shipContainer = document.createElement('div');
+    shipContainer.classList.add("info-shipcontainer");
+    let shipName = document.createElement("div");
+    shipName.classList.add("info-shipname");
+    shipName.textContent = inputName;
+    let shipHealth = document.createElement("div");
+    shipHealth.classList.add("info-shiphealth");
+
+    shipContainer.appendChild(shipName);
+    shipContainer.appendChild(shipHealth);
+    infoShip.appendChild(shipContainer);
+}
+
 function initCompGame(){
     let p1 = new Player();
     p1.playerType = "Human";
     p1.name = prompt("Enter your name","Player");
     
+    P1InfoPlacer(p1);
+
     for(let i = 0; i < 100; i++){
         P1TileMaker(i);
         P2TileMaker(i);
     }
+
+    initP1ShipPlacement();
+}
+
+function initP1ShipPlacement(){
+
 }
 
 export {initCompGame}
--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,9 @@
+import { Ship } from "./ship.js";
+
 class Gameboard{
     constructor(){
         this.board = [];
+        this.ships = [];
 
         if(!this.board || !this.board.length){
             this.initializeBoard();
@@ -10,6 +13,14 @@ class Gameboard{
     initializeBoard(){
         for(let i = 0; i < 100; i++){
             this.board.push({hasShip:false,wasShot:false});
+        }
+
+        let shipNames = ["Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"];
+        let shipLengths = [5, 4, 3, 3, 2];
+
+        for(let i = 0; i < 5; i++){
+            let newShip = new Ship(shipNames[i],shipLengths[i]);
+            this.ships.push(newShip);
         }
     }
 
