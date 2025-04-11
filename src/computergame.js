fdd516c544a4ec6a5e8b7a6b76634e7a930ba2c2
Keith Estrella <keithestrella@gmail.com>
2025-04-11 03:04:28 -0400

    Add basic piece placement system

--- src/computergame.js
+++ src/computergame.js
@@ -4,7 +4,8 @@ let p1Board = document.querySelector("#p1-gameboard");
 let p2Board = document.querySelector("#p2-gameboard");
 
 function P1TileShipPlacer(ship,shipDirection,gameboard){
-    let p1Tiles = document.querySelectorAll("div.p1-tiles");
+    let p1Tiles = document.querySelectorAll("div.p1-tile");
+    let p1board = gameboard.board;
     
     for(let i = 0; i < p1Tiles.length; i++){
         p1Tiles[i].addEventListener('click', (e) => {
@@ -14,20 +15,29 @@ function P1TileShipPlacer(ship,shipDirection,gameboard){
                 if((i%10)+ship.length > 10){
                     return alert("Invalid placement!");
                 }else{
+                    let validPlacement = true;
+                    let k = i;
                     for(let j = i; j < i+ship.length; j++){
                         let board = gameboard.board[j];
                         if(board.hasShip != false){
-                            return alert("Invalid placement! Overlapping with another ship!");
-                        }else{
-                            board.hasShip = ship;
+                            validPlacement = false;
                         }
                     }
+
+                    if(validPlacement == true){
+                        for(let m = k; m < k+ship.length; m++){
+                            let board = gameboard.board[m];
+                            board.hasShip = ship;
+                        }
+                    }else{
+                        return alert("Invalid placement! Overlapping with another ship!");
+                    }
                 }
-            }else{
+            }else if(shipDirection == "y"){
                 if((i%10)+ship.length > 10){
                     return alert("Invalid placement!");
                 }else{
-                    for(let j = 0; j < ship.length; i++){
+                    for(let j = 0; j < ship.length; j++){
                         let board = gameboard.board[i+(j*10)];
                         if(board.hasShip != false){
                             return alert("Invalid placement! Overlapping with another ship!");
@@ -37,14 +47,23 @@ function P1TileShipPlacer(ship,shipDirection,gameboard){
                     }
                 }
             }
+
+            updateP1ShipPlacement(p1board);
+            checkP1BoardSet(p1board);
         })
     }
 }
 
-function P1TileMaker(index){
+function P1TileMaker(index,tileinfo){
     let tile = document.createElement('div');
     tile.classList.add("p1-tile");
     tile.textContent = index;
+
+    let info = document.createElement('div');
+    info.classList.add("p1-tileinfo");
+    info.textContent = tileinfo;
+
+    tile.appendChild(info);
     p1Board.appendChild(tile);
 }
 
@@ -92,11 +111,15 @@ function initCompGame(){
     let p1 = new Player();
     p1.playerType = "Human";
     p1.name = prompt("Enter your name","Player");
+    let p1gb = p1.gameboard;
+    let board = p1gb.board;
     
     P1InfoPlacer(p1);
 
     for(let i = 0; i < 100; i++){
-        P1TileMaker(i);
+        let hasShip = board[i].hasShip;
+
+        P1TileMaker(i,hasShip);
         P2TileMaker(i);
     }
 
@@ -104,7 +127,77 @@ function initCompGame(){
 }
 
 function initP1ShipPlacement(p1gameboard){
-    
+    let p1gbShip = p1gameboard.ships;
+    let direction = "x";
+
+    // P1TileShipPlacer(p1gbShip[0],direction,p1gameboard);
+
+    let infoSelect = document.querySelectorAll(".info-shipcontainer");
+
+    console.log(infoSelect);
+
+    for(let i = 0; i < infoSelect.length; i++){
+        infoSelect[i].addEventListener('click', (e) => {
+            e.preventDefault();
+
+            P1TileShipPlacer(p1gbShip[i],direction,p1gameboard);
+        })
+    }
+}
+
+function updateP1ShipPlacement(board) {
+    console.log(board);
+
+    let p1GBDiv = document.querySelector("#p1-gameboard");
+
+    while(p1GBDiv.lastElementChild) {
+        p1GBDiv.removeChild(p1GBDiv.lastElementChild);
+    }
+
+    for(let i = 0; i < 100; i++){
+        let hasShip = board[i].hasShip;
+
+        if(typeof hasShip == 'object'){
+            hasShip = hasShip.shipname;
+        }
+
+        P1TileMaker(i,hasShip);
+    }
+}
+
+function checkP1BoardSet(board) {
+    let carr,bship,cruise,sub,dest = false;
+    for(let i = 0; i < board.length; i++){
+        if(typeof board[i].hasShip == 'object'){
+            let ship = board[i].hasShip;
+            let name = ship.shipname;
+
+            switch(name){
+                case 'Carrier':
+                    carr = true;
+                    break;
+                case 'Battleship':
+                    bship = true;
+                    break;
+                case 'Cruiser':
+                    cruise = true;
+                    break;
+                case 'Submarine':
+                    sub = true;
+                    break;
+                case 'Destroyer':
+                    dest = true;
+                    break;
+            }
+        }
+    }
+    if(carr == true && bship == true && cruise == true && sub == true && dest == true){
+            initStartGame();
+    }
+}
+
+function initStartGame(){
+    console.log("Ready to start match!");
 }
 
 export {initCompGame}
